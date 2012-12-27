57af8f41d8dcdb324b320a7f1ebeacb16a37d8fa
Lars Hanisch <dvb@flensrocker.de>
2012-12-27 11:50:56 +0100

    use vdr's cStringList instead of AvahiStringList

--- avahi-helper.h
+++ avahi-helper.h
@@ -1,18 +1,19 @@
 #ifndef __AVAHI_HELPER_H
 #define __AVAHI_HELPER_H
 
-#include <avahi-common/strlst.h>
+#include <vdr/tools.h>
 
 
 class cAvahiHelper
 {
 private:
-  AvahiStringList  *_list;
+  cStringList  _list;
 
 public:
+  // instantiate with an option string like "key1=value1,key2=value2,key2=value3,key3=value4,..."
+  // comma and backslashes in values must be escaped with a backslash
   cAvahiHelper(const char *Options)
   {
-    _list = NULL;
     if (Options != NULL) {
        char *s = strdup(Options);
        size_t len = strlen(Options);
@@ -34,86 +35,66 @@ public:
              if ((s[pos2] == ',') && (s[pos2 - 1] != '\\')) {
                 s[pos2] = 0;
                 dsyslog("avahi4vdr-helper: add '%s'", s + pos1);
-                if (_list == NULL)
-                   _list = avahi_string_list_new(s + pos1, NULL);
-                else
-                   _list = avahi_string_list_add(_list, s + pos1);
+                _list.Append(strdup(s + pos1));
                 pos1 = pos2 + 1;
                 }
              pos2++;
              }
        if (pos2 > pos1) {
           dsyslog("avahi4vdr-helper: add '%s'", s + pos1);
-          if (_list == NULL)
-             _list = avahi_string_list_new(s + pos1, NULL);
-          else
-             _list = avahi_string_list_add(_list, s + pos1);
+          _list.Append(strdup(s + pos1));
           }
        free(s);
-       // with avahi_string_list_add new entries are added to the beginning
-       // so reverse order to get the parameters in the right order
-       if (_list != NULL)
-          _list = avahi_string_list_reverse(_list);
        }
   };
 
-  ~cAvahiHelper(void)
-  {
-    if (_list != NULL) {
-       avahi_string_list_free(_list);
-       _list = NULL;
-       }
-  };
-
+  // if Number is greater than zero, returns the n'th appearance of the parameter
   const char *Get(const char *Name, int Number = 0)
   {
-    if (_list == NULL)
-       return NULL;
     if (Name == NULL)
        return NULL;
     size_t name_len = strlen(Name);
     if (name_len == 0)
        return NULL;
 
-    AvahiStringList *l = _list;
-    while (l != NULL) {
-          size_t len = avahi_string_list_get_size(l);
-          if (len > name_len) {
-             const char *text = (const char*)avahi_string_list_get_text(l);
-             if ((strncmp(text, Name, name_len) == 0) && (text[name_len] == '=')) {
-                Number--;
-                if (Number < 0) {
-                   dsyslog("avahi4vdr-helper: found parameter '%s'", text);
-                   return text + name_len + 1;
-                   }
-                }
-             }
-          l = avahi_string_list_get_next(l);
-          }
+    for (int i = 0; i < _list.Size(); i++) {
+        const char *text = _list[i];
+        if (text == NULL)
+           continue;
+        size_t len = strlen(text);
+        if (len > name_len) {
+           if ((strncmp(text, Name, name_len) == 0) && (text[name_len] == '=')) {
+              Number--;
+              if (Number < 0) {
+                 dsyslog("avahi4vdr-helper: found parameter '%s'", text);
+                 return text + name_len + 1;
+                 }
+              }
+           }
+        }
     return NULL;
   };
 
+  // get the number of appearances of parameter "Name"
   int Count(const char *Name)
   {
-    int count = 0;
-    if (_list == NULL)
-       return count;
     if (Name == NULL)
-       return count;
+       return 0;
     size_t name_len = strlen(Name);
     if (name_len == 0)
-       return count;
+       return 0;
 
-    AvahiStringList *l = _list;
-    while (l != NULL) {
-          size_t len = avahi_string_list_get_size(l);
-          if (len > name_len) {
-             const char *text = (const char*)avahi_string_list_get_text(l);
-             if ((strncmp(text, Name, name_len) == 0) && (text[name_len] == '='))
-                count++;
-             }
-          l = avahi_string_list_get_next(l);
-          }
+    int count = 0;
+    for (int i = 0; i < _list.Size(); i++) {
+        const char *text = _list[i];
+        if (text == NULL)
+           continue;
+        size_t len = strlen(text);
+        if (len > name_len) {
+           if ((strncmp(text, Name, name_len) == 0) && (text[name_len] == '='))
+              count++;
+           }
+        }
     return count;
   };
 };
